6e6a96d8b90f9b2295340c7da45361ae9f3f7891
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-25 11:32:39 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import agent from './agent.js';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/generate', (req, res) => {
+    const { query, video_id } = req.body;
+    console.log(query, video_id);
+
+    res.send('Hello.......')
 });
 
 app.listen(port, () => {
